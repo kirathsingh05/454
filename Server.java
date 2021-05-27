536ac118902c0a5c06c4be465a128ff4d5cb3aeb
Kirath <kirathsingh05@gmail.com>
2021-05-27 16:36:48 -0400

    fixed second loop

--- Server.java
+++ Server.java
@@ -7,7 +7,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-//ttmn
 class Server {
 
 	static HashMap<Integer, Integer> edges = new HashMap<Integer, Integer>();
@@ -31,66 +30,70 @@ class Server {
 		}
 	}
 
-
-	// MAIN
 	public static void main(String args[]) throws Exception {
 		if (args.length != 1) {
 			System.out.println("usage: java Server port");
-			// open
-			// --
-			int port = Integer.parseInt(args[0]);
-			ServerSocket ssock = new ServerSocket(port);
-			System.out.println("listening on port " + port);
-			// --
-			Socket csock = ssock.accept();
-			System.out.println("accepted connection " + csock);
+			System.exit(-1);
+		}
+		int port = Integer.parseInt(args[0]);
 
-			DataInputStream din = new DataInputStream(csock.getInputStream());
-			int dataLength = din.readInt();
-			System.out.println("received request header, data payload has length " + dataLength);
-			byte[] bytes = new byte[dataLength];
-			din.readFully(bytes);
-
-			int i = 0;
-			while (i < bytes.length) { 
-				try {
+		ServerSocket ssock = new ServerSocket(port);
+		System.out.println("listening on port " + port);
+		while(true) {
+			try {
+				Socket csock = ssock.accept();
+				System.out.println("Accepted connection: " + csock);
+				
+				//read request into byte array
+				DataInputStream din = new DataInputStream(csock.getInputStream());
+				int reqDataLen = din.readInt();
+				System.out.println("received request header, data payload has length " + reqDataLen);
+				byte[] bytes = new byte[reqDataLen];
+				din.readFully(bytes);
+				
+				int i = 0;
+				while (i < bytes.length) {
+					// decl.
 					int node1;
 					int node2;
-					// #1
+					// assn.
 					node1 = 0;
 					node2 = 0;
+					// read first vertex
 					while(bytes[i] != 32){
-						node1 = 10*node1 + (bytes[i] - 48) ; 	//48 offs.
+						node1 = node1*10 + (bytes[i]-48) ;
 						i++;
 					}
 					i++;
-					while(bytes[i] !=10 ){ 						//#2
-						node2 = (bytes[i] - 48) + 10*node2 ;
+					// get second vertex
+					while(bytes[i] !=10 ){
+						node2 = node2*10 + (bytes[i]-48) ;
 						i++;
 					}
 					i++;
 					updateValues(node1,node2);
-				} catch (Exception e) {
-				e.printStackTrace();
 				}
+				String result = new String();
+				HashMap<Integer, Integer> temp = edges;
+				StringBuilder sb = new StringBuilder();
+				for (int key : temp.keySet()) {
+		        	sb.append(key);
+		        	sb.append(" ");
+		        	sb.append(temp.get(key));   
+		    	}
+				result = sb.toString();
+				
+				// append result
+				DataOutputStream dout = new DataOutputStream(csock.getOutputStream());
+				bytes = result.getBytes("UTF-8");
+				dout.writeInt(bytes.length);
+				dout.write(bytes);
+				dout.flush();
+				System.out.println("#result data (in bytes): " + bytes.length);
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			String result = new String();
-			HashMap<Integer, Integer> temp = edges;
-			StringBuilder sb = new StringBuilder();
-			for (int key : temp.keySet()) {
-	        	sb.append(key);
-	        	sb.append(" ");
-	        	sb.append(temp.get(key));   
-	    	}
-			result = sb.toString();
+		}
 
-			// Write result to the client
-			DataOutputStream dout = new DataOutputStream(csock.getOutputStream());
-			bytes = result.getBytes("UTF-8");
-			dout.writeInt(bytes.length);
-			dout.write(bytes);
-			dout.flush();
-			System.out.println("#result data (in bytes): " + bytes.length);
-		} 
 	}
 }
